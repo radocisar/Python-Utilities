605518e8fa9dcecfc4b7a4eb97b80d738de395d3
Rado Cisar <32033092+radocisar@users.noreply.github.com>
2018-09-21 19:37:35 -0400

    Update Decompression_of_Files.py

--- Decompression_of_Files.py
+++ Decompression_of_Files.py
@@ -2,5 +2,13 @@
 
 import patoolib
 
+# For a single file:
 patoolib.extract_archive(r"C:\Raw_Data\Raw_1_sec_Bar_Data\US_Stocks\MSFT\MSFT_Bars_20180913.txt.gz", 
                           outdir=r"C:\Raw_Data\Raw_1_sec_Bar_Data\US_Stocks\MSFT\M\\")
+
+# For all files in folder:
+for files in os.listdir(r"C:\Raw_Data\Raw_1_sec_Bar_Data\US_Stocks\GOOGL\\"):
+    #print(files)
+    patoolib.extract_archive(r"C:\Raw_Data\Raw_1_sec_Bar_Data\US_Stocks\GOOGL\{}".format(files), 
+                             outdir=r"C:\Raw_Data\Raw_1_sec_Bar_Data\US_Stocks\GOOGL\\")
+    os.remove(r"C:\Raw_Data\Raw_1_sec_Bar_Data\US_Stocks\GOOGL\{}".format(files))
